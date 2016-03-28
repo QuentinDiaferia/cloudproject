0f334eca02f87fb5019a083b79159de20360dd17
QuentinDiaferia <q.diaferia@gmail.com>
2016-03-28 22:25:34 +0100

    landing pge + dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -18,47 +18,8 @@
     </div>
     <!-- /.row -->
     <div class="row">
-    	<div class="col-lg-3">
-    		<div class="panel panel-primary">
-    			<div class="panel-heading">
-    				My Groups
-    			</div>
-    			<div class="panel-body">
-    				<div class="panel panel-info">
-    					<div class="panel-heading">
-    						Group project
-    					</div>
-    					<div class="panel-body">
-    						with Machin Truc and Bidule Chouette<br />
-    						3 files
-    					</div>
-    				</div>
-
-    				<div class="panel panel-info">
-    					<div class="panel-heading">
-    						Management
-    					</div>
-    					<div class="panel-body">
-    						with Machin Truc and Bidule Chouette<br />
-    						3 files
-    					</div>
-    				</div>
-    			</div>
-    		</div>
-    	</div>
-    	<div class="col-lg-3">
-    		<div class="panel panel-green">
-    			<div class="panel-heading">
-    				Last activities
-    			</div>
-    			<div class="panel-body">
-    				Machin Truc added file.txt to <a href="">Group project</a>.<br />
-    				<p class="small text-right">16 minutes ago.</p>
-    				<hr />
-    				Bidule Chouette updated file.txt to <a href="">Management</a>.<br />
-    				<p class="small text-right">3 hours ago.</p>
-    			</div>
-    		</div>
+    	<div class="col-lg-12">
+    		<h3>Select a group or create a new one in the menu to get started !</h3>
     	</div>
     </div>
 @stop
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,5 +10,14 @@
             <h1 class="page-header">Welcome !</h1>
         </div>
         <!-- /.col-lg-12 -->
+        <h2>How to use this website</h2>
+        <p>Could it be any easier ?</p>
+        <ul>
+        	<li>Register</li>
+        	<li>Login</li>
+        	<li>Create a group</li>
+        	<li>Add your colleagues to the group</li>
+        	<li>Upload files</li>
+        </ul>
     </div>
 @endsection
