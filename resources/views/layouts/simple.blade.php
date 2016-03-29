06ec86ad504018ccc6a46cdd77282e43b115aeb9
QuentinDiaferia <q.diaferia@gmail.com>
2016-03-29 14:35:52 +0100

    paths

--- resources/views/layouts/simple.blade.php
+++ resources/views/layouts/simple.blade.php
@@ -35,7 +35,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="/cloudcomputing/public/">App Name</a>
+                <a class="navbar-brand" href="{{ url('/') }}">App Name</a>
             </div>
             <!-- /.navbar-header -->
         </nav>
